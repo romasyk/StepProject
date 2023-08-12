2c7248338b5fa8bd70e2c7835ea010e6aebe6a02
andriistebeletskyi <andriysteb.2005@gmail.com>
2023-08-12 22:19:38 +0300

    1

--- project/src/structure/dao/FlightsDao.java
+++ project/src/structure/dao/FlightsDao.java
@@ -1,4 +1,13 @@
 package structure.dao;
 
+import structure.model.Flight;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FlightsDao {
+    public static List<Flight> flightList = new ArrayList<>();
+    public List<Flight> getAllFlights() {
+        return flightList;
+    }
 }
--- project/src/structure/model/Flight.java
+++ project/src/structure/model/Flight.java
@@ -1,5 +1,60 @@
 package structure.model;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Flight {
+    private String id;
+    private LocalDateTime dateTime;
+    private Destination destination;
+    private PlaceOfDeparture departure;
+    private int seats;
 
+    public Flight(PlaceOfDeparture departure, String id, LocalDateTime dateTime, Destination destination, int seats) {
+        this.departure = departure;
+        this.id = id;
+        this.dateTime = dateTime;
+        this.destination = destination;
+        this.seats = seats;
+    }
+
+    public PlaceOfDeparture getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(PlaceOfDeparture departure) {
+        this.departure = departure;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Destination getDestination() {
+        return destination;
+    }
+
+    public void setDestination(Destination destination) {
+        this.destination = destination;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
 }
